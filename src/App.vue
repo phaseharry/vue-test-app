f62ce12994857dd254008755286069fbfb0c9284
Harry C <harrychenhc@icloud.com>
2019-07-02 12:36:14 -0400

    simple setup and changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- v-bind:"todos" is passing our todos data as a "todos" prop to the Todos component -->
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from "./components/Todos";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    // adding the list of components that will be directly rendered by our App component
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "Todo One", completed: false },
+        { id: 2, title: "Todo Two", completed: false },
+        { id: 3, title: "Todo Three", completed: true }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,20 @@
+<!-- anything wrapped around the template element represents the markup we render on the screen -->
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <h3>{{ todo.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+/* we're exporting this component named "Todos" so that other components can use it */
+export default {
+  name: "Todos",
+  props: ["todos"] // accepting the "todos" prop that's being passed from the main app component
+};
+</script>
+
+<style scoped>
+/* adding the scoped attribute will apply these styles only to this component */
+</style>
